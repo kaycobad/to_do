c51e8c63971d93857578c7e259db2f02b12293e9
kaycobadJony <kaycobadjony@live.com>
2022-02-21 21:21:34 +0600

    new2

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 
 import '../providers/tasks_provider.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -12,7 +11,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   void initState() {
     // TODO: implement initState
@@ -24,6 +22,46 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            pinned: true,
+            snap: false,
+            centerTitle: false,
+            automaticallyImplyLeading: false,
+            title: const Text('ToDo'),
+            bottom: AppBar(
+              toolbarHeight: 70,
+              automaticallyImplyLeading: false,
+              title: Consumer<TasksProvider>(
+                builder: (BuildContext context, value, Widget? child) {
+                  return Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      '${value.tasks.length} TASKS',
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+      // body: Consumer<TasksProvider>(builder: (BuildContext context, value, Widget? child) {
+      //   if(value.tasks.isEmpty) {
+      //     return const Center(
+      //       child: Text('No Tasks'),
+      //     );
+      //   } else {
+      //     return Text('');
+      //   }
+      // },),
+    );
   }
 }
